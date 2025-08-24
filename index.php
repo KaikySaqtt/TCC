de5a830fc7f87a563c8b7d3985acfb070fbf7716
Kaikysaqtt <kaikysaquette603@gmail.com>
2025-08-24 19:13:37 -0300

    Finalização da home, com a responsividade

--- index.php
+++ index.php
@@ -1,136 +1,166 @@
 <?php
 require "config.php";
 include_once DBAPI;
-if (!isset($_SESSION))
-    session_start();
+if (!isset($_SESSION)) session_start();
 include(HEADER_TEMPLATE);
 $db = open_database();
 ?>
 <?php if ($db): ?>
     <?php if (!empty($_SESSION['message'])): ?>
-        <div class="alert alert-<?php echo $_SESSION['type']; ?> alert-dismissible" role="alert">
-            <?php echo $_SESSION['message']; ?></>
+        <div class="alert alert-<?= $_SESSION['type']; ?> alert-dismissible" role="alert">
+            <?= $_SESSION['message']; ?>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-    <?php endif; ?>
-
-    <?php clear_messages(); ?>
+    <?php endif;
+    clear_messages(); ?>
 <?php endif; ?>
-<div style="position: relative;">
-    <div class="int">
-        <div class="esquerda">
-            <h1 class="agrandir"> <b> Um restaurante em sua residência </b></h1>
-            <h4 class="agrandir">Desfrute de uma experiência culinária personalizada <br>
-                com a personal chef Karol Marques <br>
-                tudo no conforto da sua própria casa! </h4>
-             <div class="d-flex justify-content-center mt-2">
-                <a href="#" class="btn btn-km">Começar</a>
-             </div>
-        </div>
-        <div class="direita">
-            <div class="container">
-                <div class="row flex-nowrap int-cards">
-                    <div class="col-sm col-left">
-                        <div class="card card-left cardtopo">
-                            <img class="card-img-top rounded" src="kmimagens/km1.png" alt="Imagem de capa do card">
-                        </div>
-                        <div class="card card-left">
-                            <img class="card-img-top rounded" src="kmimagens/docepote.png" alt="Imagem de capa do card">
-                        </div>
-                        <div class="card card-left">
-                            <img class="card-img-top rounded" src="kmimagens/marmitas42.png" alt="Imagem de capa do card">
-                        </div>
-                    </div>
-                    <div class="col-sm col-meio">
-                        <div class="card card-meio cardtopo" >
-                            <img class="card-img-top rounded" src="kmimagens/docepote4.png" alt="Imagem de capa do card">
-                        </div>
-                        <div class="card card-meio">
-                            <img class="card-img-top rounded" src="kmimagens/km2.png" alt="Imagem de capa do card">
-                        </div>
-                        <div class="card card-meio">
-                            <img class="card-img-top rounded" src="kmimagens/marmitas4.png" alt="Imagem de capa do card">
-                        </div>
-                    </div>
-                    <div class="col-sm col-right">
-                        <div class="card card-rigth cardtopo">
-                            <img class="card-img-top rounded" src="kmimagens/comida1.png" alt="Imagem de capa do card">
-                        </div>
-                        <div class="card card-rigth">
-                            <img class="card-img-top rounded" src="kmimagens/docepote2.png" alt="Imagem de capa do card">
-                        </div>
-                        <div class="card card-rigth">
-                            <img class="card-img-top rounded" src="kmimagens/comida3.png" alt="Imagem de capa do card">
-                        </div>
-                    </div>
-                </div>
-            </div>
 
+<!-- INT / HERO -->
+<section class="int container py-5">
+    <div class="row align-items-center g-4">
+        <div class="col-lg-6 text-center text-lg-start esquerda">
+            <h1 class="agrandir fw-bold">Um restaurante em sua residência</h1>
+            <h4 class="agrandir lh-base">
+                Desfrute de uma experiência culinária personalizada
+                <br class="d-none d-lg-inline">com a personal chef Karol Marques
+                <br class="d-none d-lg-inline">tudo no conforto da sua própria casa!
+            </h4>
+            <a href="#" class="btn btn-km btn-lg mt-3">Começar</a>
+        </div>
+
+        <div class="col-lg-6 direita">
+            <!-- Grid de cards (mosaico estático em HTML) -->
+            <div class="row g-3 row-cols-2 row-cols-md-3 gallery">
+                <div class="col col-responsive">
+                    <div class="card shadow-sm h-100">
+                        <div class="ratio ratio-1x1">
+                            <img src="kmimagens/km1.png" class="w-100 h-100" alt="Imagem 1">
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm h-100">
+                        <div class="ratio ratio-1x1">
+                            <img src="kmimagens/docepote.png" class="w-100 h-100" alt="Imagem 2">
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm h-100">
+                        <div class="ratio ratio-1x1">
+                            <img src="kmimagens/marmitas42.png" class="w-100 h-100" alt="Imagem 3">
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-responsive">
+                    <div class="card shadow-sm h-100">
+                        <div class="ratio ratio-1x1">
+                            <img src="kmimagens/docepote4.png" class="w-100 h-100" alt="Imagem 4">
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm h-100">
+                        <div class="ratio ratio-1x1">
+                            <img src="kmimagens/km2.png" class="w-100 h-100" alt="Imagem 5">
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-responsive">
+                    <div class="card shadow-sm h-100">
+                        <div class="ratio ratio-1x1">
+                            <img src="kmimagens/marmitas4.png" class="w-100 h-100" alt="Imagem 6">
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm h-100">
+                        <div class="ratio ratio-1x1">
+                            <img src="kmimagens/comida1.png" class="w-100 h-100" alt="Imagem 7">
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm h-100">
+                        <div class="ratio ratio-1x1">
+                            <img src="kmimagens/docepote2.png" class="w-100 h-100" alt="Imagem 8">
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card shadow-sm h-100">
+                        <div class="ratio ratio-1x1">
+                            <img src="kmimagens/comida3.png" class="w-100 h-100" alt="Imagem 9">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<!-- SERVICES -->
+<section class="services py-5">
+    <div class="container bg-white rounded-3 p-4 p-md-5 shadow-sm">
+        <div class="row align-items-center g-4">
+            <div class="col-lg-5">
+                <h4 class="agrandir text-center text-lg-start">
+                    Descubra nossos serviços especializados e personalizados para você!
+                </h4>
+            </div>
+            <div class="col-lg-7">
+                <div class="row g-4 row-cols-1 row-cols-sm-2">
+                    <div class="col">
+                        <div class="card h-60 shadow-sm">
+                            <img src="kmimagens/comida.png" class="card-img-top" alt="Jantares privativos">
+                            <div class="card-body text-center">
+                                <h5 class="card-title">Jantares privativos</h5>
+                                <a href="#" class="btn btn-km">Ver mais</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100 shadow-sm">
+                            <img src="kmimagens/marmita2.png" class="card-img-top" alt="Marmitas e finger food">
+                            <div class="card-body text-center">
+                                <h5 class="card-title">Marmitas e finger food</h5>
+                                <a href="#" class="btn btn-km">Ver mais</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="services d-flex justify-content-center align-items-center">
-        <section class="container rounded">
-            <div class="tituloeventos">
-                <h4 class="agrandir">Descubra nossos serviços especializados e personalizados para você!</h4>
+</section>
+
+<!-- SOBRE KM -->
+<section class="sobre-km py-5">
+    <div class="container rounded-3 p-4 p-md-5">
+        <div class="row align-items-center g-4">
+            <div class="col-lg-5">
+                <h3 class="agrandir">
+                    Conheça mais da nossa personal chef Karol Marques, especialista em gastronomia
+                </h3>
+                <a href="#" class="btn btn-km mt-3">Ver mais</a>
             </div>
-            <div class="eventos d-flex align-items-center">
-                <div class="row flex-nowrap">
-                    <div class="col-sm">
-                        <div class="card">
-                            <div class="card-body ">
-                                <h5 class="card-title text-center">Jantares privativos</h5>
-                                <img class="card-img" src="kmimagens/comida.png" alt="Imagem de capa do card">
-                                <div class="d-flex justify-content-center mt-2">
-                                    <a href="#" class="btn btn-km">Ver mais</a>
-                                </div>
-                            </div>
+            <div class="col-lg-7">
+                <div class="row g-3 row-cols-2">
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <img class="card-img-top" src="kmimagens/km6.png" alt="Karol 1">
                         </div>
                     </div>
-                    <div class="col-sm">
-                        <div class="card">
-                            <div class="card-body ">
-                                <h5 class="card-title text-center">Marmitas e finger food</h5>
-                                <img class="card-img" src="kmimagens/marmita2.png" alt="Imagem de capa do card">
-                                <div class="d-flex justify-content-center mt-2">
-                                    <a href="#" class="btn btn-km">Ver mais</a>
-                                </div>
-                            </div>
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <img class="card-img-top" src="kmimagens/km3.png" alt="Karol 2">
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     </div>
-</div>
-<div class="sobre-km d-flex justify-content-center align-items-center">
-        <section class="container rounded">
-            <div class="kmesquerda d-flex align-items-center">
-                <div class="container flex-column  mb-2 ">
-                    <h3 class="agrandir">Conheça mais da nossa personal chef Karol Marques, especialista em gastronomia </h3>
-                    <div class="">
-                        <a href="#" class="btn btn-km">Ver mais</a>
-                    </div>
-                </div>
-              
-            </div>
-            <div class="kmdireita d-flex align-items-center" style="margin-bottom: 30vh;">
-                <div class="row flex-nowrap">
-                    <div class="col-sm">
-                        <div class="card">
-                            <div class="card-body">
-                                <img class="card-img img-km" src="kmimagens/km6.png" alt="Imagem de capa do card">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm">
-                        <div class="card">
-                            <div class="card-body">
-                                <img class="card-img img-km" src="kmimagens/km3.png" alt="Imagem de capa do card">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
+</section>
+
 <?php include(FOOTER_TEMPLATE); ?>
